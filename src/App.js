2674f4b6601a69873e15c2e3078c54ea86a30bf7
anjuachulekshmi <anjuachulekshmi836@gmail.com>
2022-05-18 09:03:38 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addpost from './Component/Addpost';
 import Searchpost from './Component/Searchpost';
+import Viewallpost from './Component/Viewallpost';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Searchpost />
+      <BrowserRouter>
+   <Routes>
+     <Route path='/' exact element={<Addpost/>} />
+
+     
+     <Route path='/search' exact element={<Searchpost/>} />
+     <Route path='/viewallpost' exact element={<Viewallpost/>} />
+
+    
+     </Routes>
+     </BrowserRouter>
+   
     </div>
   );
 }
